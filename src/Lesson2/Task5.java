505b54fc6d1ae004461e2953e2fad0ff710ff332
alexx5 <alexkhx5@yandex.ru>
2020-09-08 01:13:10 +0300

    Added Task5.java

--- src/Lesson2/Task5.java
+++ src/Lesson2/Task5.java
@@ -1,4 +1,25 @@
 package Lesson2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Task5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList list = new ArrayList<Integer>();
+
+        for (int i = 1; i < 4; i++) {
+            System.out.print("Введите целое число № " + i + ": ");
+            list.add(sc.next());
+        }
+
+        System.out.println("Наименьшее число: " + Collections.min(list));
+        sc.close();
+    }
+
+
+
+
 }
